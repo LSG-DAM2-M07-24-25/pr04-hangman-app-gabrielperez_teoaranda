d1042ec649dcea00ef286334938aa5d6c7575c65
ElDark84 <157355717+ElDark84@users.noreply.github.com>
2024-12-11 20:05:47 +0100

    Update MenuScreen.kt

--- app/src/main/java/com/example/hangman_gabrielperez_teoaranda/MenuScreen.kt
+++ app/src/main/java/com/example/hangman_gabrielperez_teoaranda/MenuScreen.kt
@@ -16,6 +16,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -28,6 +30,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -59,6 +62,9 @@ class MenuScreen : ComponentActivity() {
 
 @Composable
 fun Screen2(modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false)}
+    var selectedOption by remember { mutableStateOf("Selecciona la dificultat") }
+    val options = listOf("facil", "medio", "difícil")
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -90,10 +96,6 @@ fun Screen2(modifier: Modifier = Modifier) {
         }
     }
 }
-
-
-
-
 
 @Preview(showBackground = true)
 @Composable
